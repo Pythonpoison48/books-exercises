68ff51cc4600c42aad5e1ad65a868eb958b945c6
Konata <konata@member.fsf.org>
2026-09-16 10:49:46 +0200

    Updating exercises for chapter 4

---
+++ intermediate-perl-2th/ex4-2.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+my @skipper   = qw(blue_shirt hat jacket preserver sunscreen);
+my @professor = qw(sunscreen water_bottle slide_rule batteries radio);
+my @gilligan  = qw(red_shirt hat lucky_socks water_bottle);
+
+my %all = (
+    Gilligan  => \@gilligan,
+    Skipper   => \@skipper,
+    Professor => \@professor,
+);
+
+Dumper( %{ \%all } );
+
+check_items_for_all( \%all );
+
+sub check_items_for_all {
+    my %hash = %{ $_[0] };
+    map { &check_required_items( $_, %hash{$_} ) } keys %hash;
+}
+
+sub check_required_items {
+    my $who   = shift;
+    my $items = shift;
+
+    my %whose_items = map { $_, 1 } @$items;
+
+    my @required = qw(preserver sunscreen water_bottle jacket);
+    my @missing  = ();
+
+    for my $item (@required) {
+        unless ( $whose_items{$item} ) {    # not found in list?
+            print "$who is missing $item.\n";
+            push @missing, $item;
+        }
+    }
+
+    if (@missing) {
+        print "Adding @missing to @$items for $who.\n";
+        push @$items, @missing;
+    }
+}
